0d6df22fc0b86591519566931cc393e55b949df3
ncmarcal <nathancori@hotmail.com>
2025-01-28 19:40:04 -0300

    style: centralizar div que segura a imagem

--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -4,10 +4,11 @@ import { InputTextModule } from 'primeng/inputtext';
 import { IftaLabelModule } from 'primeng/iftalabel';
 import { ButtonModule } from 'primeng/button';
 import { FloatLabelModule } from 'primeng/floatlabel';
+import { DividerModule } from 'primeng/divider';
 
 @Component({
   selector: 'app-login',
-  imports: [CardModule, InputTextModule, IftaLabelModule, ButtonModule, FloatLabelModule],
+  imports: [CardModule, InputTextModule, IftaLabelModule, ButtonModule, FloatLabelModule, DividerModule],
   templateUrl: './login.component.html',
   styleUrl: './login.component.scss'
 })
