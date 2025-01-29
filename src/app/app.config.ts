bad4d9b2da428e19dc82f44e7b4c078fd31b1b9b
ncmarcal <nathancori@hotmail.com>
2025-01-29 18:24:29 -0300

    feat: criar authguard para validação de acesso com token

--- src/app/app.config.ts
+++ src/app/app.config.ts
@@ -1,10 +1,11 @@
 import { ApplicationConfig, provideZoneChangeDetection } from '@angular/core';
-import { provideRouter } from '@angular/router';
+import { provideRouter, RouterModule } from '@angular/router';
 import { provideAnimationsAsync } from '@angular/platform-browser/animations/async'
 import { providePrimeNG } from 'primeng/config';
 import { routes } from './app.routes';
 import MyPreset from './mypreset';
 import { provideHttpClient } from '@angular/common/http';
+import { MessageService } from 'primeng/api';
 
 export const appConfig: ApplicationConfig = {
   providers: [provideZoneChangeDetection({ eventCoalescing: true }), provideRouter(routes), provideAnimationsAsync(), providePrimeNG({
@@ -14,5 +15,5 @@ export const appConfig: ApplicationConfig = {
         darkModeSelector: false || 'none'
       }
     }
-  }), provideHttpClient()]
+  }), provideHttpClient(), MessageService]
 };
--- src/app/login/login.component.ts
+++ src/app/login/login.component.ts
@@ -1,4 +1,4 @@
-import { Component, inject } from '@angular/core';
+import { Component, Inject, inject } from '@angular/core';
 import { CardModule } from 'primeng/card';
 import { InputTextModule } from 'primeng/inputtext';
 import { IftaLabelModule } from 'primeng/iftalabel';
@@ -8,6 +8,9 @@ import { DividerModule } from 'primeng/divider';
 import { FormBuilder, ReactiveFormsModule, Validators } from '@angular/forms';
 import { RequisicaoLoginModel } from './model/requisicao-login.model';
 import { LoginService } from './login.service';
+import { Router } from '@angular/router';
+import { MessageService } from 'primeng/api';
+import { HttpErrorResponse } from '@angular/common/http';
 
 @Component({
   selector: 'app-login',
@@ -19,7 +22,8 @@ export class LoginComponent {
 
   private fb = inject(FormBuilder);
   private service = inject(LoginService);
-
+  private router = inject(Router);
+  private messageService = inject(MessageService)
 
   protected formLogin = this.fb.group({
     usuario: ['', Validators.required],
@@ -33,10 +37,12 @@ export class LoginComponent {
     requisicaoLogin.password = this.formLogin.value.senha ?? '';
     this.service.login(requisicaoLogin).subscribe({
       next: (resposta) => {
-        console.log(resposta);
+        this.messageService.add({ severity: 'success', summary: 'Success', detail: 'Login realizado com sucesso!' });
+        sessionStorage.setItem("auth-token", Object.values(resposta).toString());
+        this.router.navigate(['']);
       },
-      error: (erro) => {
-        console.log(erro);
+      error: (erro: HttpErrorResponse) => {
+        this.messageService.add({ severity: 'error', summary: 'Error', detail: erro.error });
       }
     })
   }
