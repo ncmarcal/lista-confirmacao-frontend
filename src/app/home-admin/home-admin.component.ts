1b5c839dd9abeca0e30bd44dc32fe34babee977d
ncmarcal <nathancori@hotmail.com>
2025-02-10 19:30:30 -0300

    feat: implementar filtro na tabela de usuários

--- src/app/home-admin/home-admin.component.ts
+++ src/app/home-admin/home-admin.component.ts
@@ -1,22 +1,24 @@
-import { AfterViewInit, Component, inject, OnInit } from '@angular/core';
+import { InputTextModule } from 'primeng/inputtext';
+import { AfterViewInit, Component, inject, OnInit, ViewChild } from '@angular/core';
 import { TokenService } from '../shared/services/token/token.service';
 import { CardModule } from 'primeng/card';
 import { ButtonModule } from 'primeng/button';
 import { DividerModule } from 'primeng/divider';
-import { TableModule } from 'primeng/table';
+import { Table, TableModule } from 'primeng/table';
 import { HomeAdminService } from './home-admin.service';
 import { UsuariosModel } from './model/usuarios.model';
 import { HttpErrorResponse } from '@angular/common/http';
 import { MessageService } from 'primeng/api';
+import { IconFieldModule } from 'primeng/iconfield';
+import { InputIconModule } from 'primeng/inputicon';
 
 @Component({
   selector: 'app-home-admin',
-  imports: [CardModule, ButtonModule, DividerModule, TableModule],
+  imports: [CardModule, ButtonModule, DividerModule, TableModule, IconFieldModule, InputIconModule, InputTextModule],
   templateUrl: './home-admin.component.html',
   styleUrl: './home-admin.component.scss'
 })
 export class HomeAdminComponent implements OnInit {
-
 
   usuariosModel = new Array<UsuariosModel>;
 
@@ -47,6 +49,12 @@ export class HomeAdminComponent implements OnInit {
   sair(): void {
     this.tokenService.logout();
     window.location.reload();
+  }
+
+  filtrar(event: Event): string {
+    const inputElement = event.target as HTMLInputElement;
+    const inputValue = inputElement.value;
+    return inputValue;
   }
 
 }
